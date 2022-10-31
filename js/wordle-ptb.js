a2062fd9f775f4087f3ff0ff9ac8b29d14f3861f
Rodrigo Sotolani <rsotolani@gmail.com>
2022-10-31 17:11:12 -0300

    inicio do index.html e classe do jogo

---
+++ js/wordle-ptb.js
@@ -0,0 +1,43 @@
+//Classe do jogo
+
+class Wordle {
+
+    constructor() {
+        this.rodadas = 6;
+        this.rodadaAtual = 1;
+        this.palavraSecreta = "";
+        this.tentativas = {
+            tentativa1: [],
+            tentativa2: [],
+            tentativa3: [],
+            tentativa4: [],
+            tentativa5: [],
+            tentativa6: []
+        }
+        this.ganhou = false;
+    }
+
+    //método para sortear a palavra secreta
+    sortearPalavraSecreta() {
+
+    }
+
+    //método para renderizar o board
+    renderTentativas() {
+
+    }
+
+    //método para checar a palavra-tentativa
+    checarTentativa(palavraTentativa) {
+
+    }
+
+
+}
+
+class Teclado {
+
+    constructor() {
+
+    }
+}
