c371147a93292eacc7305fe8e5fe142b0f87f28f
Rodrigo Sotolani <rsotolani@gmail.com>
2022-11-02 11:54:48 -0300

    ajustes da logica da classe e index.js dia 3

--- js/index.js
+++ js/index.js
@@ -11,6 +11,7 @@ const board = document.getElementById('board');
 const secreta = document.getElementById('secreta');
 const palpites = document.getElementById('palpites');
 const nomeJogador = document.getElementById('nome-jogador');
+let posicao = document.getElementById('11'); //primeira posicao
 
 
 //sorteando a palavra secreta
@@ -26,9 +27,9 @@ btnJogar.addEventListener("click", () => {
     teclado.classList.remove("hidden");
   
     //colocando o nome do jogador na classe wordle
-    wordle.jogador = jogador.value; 
+    //wordle.jogador = jogador.value; 
     //colocando o nome do jogador no html
-    nomeJogador.innerText = wordle.jogador; 
+    //nomeJogador.innerText = wordle.jogador; 
   
   });
 
@@ -36,16 +37,70 @@ btnJogar.addEventListener("click", () => {
 
 
 
+//transform: scale(1.1)
+//transform: rotate(90deg)
 
 
-//colocando event listener no botão jogar
+//colocando event listener no botão adivinhar
+const btnAdivinhar = document.querySelector('#adivinhar');
+btnAdivinhar.onclick = () => {
+    //se pressionar Advinhar
+    if (wordle.letters.length === 5) {
+        console.log("checar palavra");
+        //chamar checar palavra
+        
+        //mudar a rodada
+        wordle.rodadaAtual++;
+        wordle.posicao = wordle.posicao + 10;
+    }
+}
+
+const btnApagar = document.querySelector('#apagar');
+btnApagar.onclick = () => {
+    if ((wordle.letters.length <= 5) || (wordle.letters.length > 0)) {
+        console.log("apagar a tecla");
+    }
+}
+
+
 
 //colocando os event listeners no teclado 
 const letras = document.querySelectorAll('.tecla');
 letras.forEach( (letra) => {
     letra.onclick = () => {
-        //console.log(letra.innerText);
-        console.log(wordle.addLetra(letra.innerText));
+
+        if (wordle.letters.length === 5) {
+            return;
+        }
+
+        //se estiver entre a rodada 1 e 6:
+        if (wordle.rodadaAtual <= wordle.rodadas) {
+            console.log("rodada", wordle.rodadaAtual);
+            console.log("posicao", wordle.palpites[wordle.rodadaAtual].length);
+            console.log("palpite", wordle.palpites[wordle.rodadaAtual]);
+
+            wordle.letters.push(letra.innerText);
+            console.log(wordle.letters);
+
+            //se não for a última letra
+            //if (wordle.posicao)
+
+            //se pressionar uma letra:
+
+            //preencher o html
+            posicao.innerText = letra.innerText;
+
+            
+            //obtendo a próxima posição
+            posicao = document.getElementById(wordle.proximaPosicao(wordle.posicao));
+
+        } else {
+            //o jogo terminou
+        }
+        
+        
+        
+
     }
 });
 
--- js/wordle-ptb.js
+++ js/wordle-ptb.js
@@ -7,22 +7,18 @@ class Wordle {
         this.rodadas = 6;
         this.rodadaAtual = 1;
         this.palavraSecreta = "";
-        this.palpites =[
-            [1, "aaaaa"],
-            [2, "bc"],
-            [3, "def"],
-            [4, "ghij"],
-            [5, "klmno"],
-            [6, ""]
-        ]
-        // this.palpites = {
-        //     palpite1: [],
-        //     palpite2: [],
-        //     palpite3: [],
-        //     palpite4: [],
-        //     palpite5: [],
-        //     palpite6: []
-        // }
+        this.palpites = ["", "", "", "", "", ""];
+        this.letters = [];
+        // this.palpites =[
+        //     [1, ""],
+        //     [2, ""],
+        //     [3, ""],
+        //     [4, ""],
+        //     [5, ""],
+        //     [6, ""]
+        // ]
+        
+        this.posicao = 11;
         this.ganhou = false;
     }
 
@@ -34,14 +30,29 @@ class Wordle {
     //método para indicar onde inserir a letra digitada
     addLetra(letra) {
        //console.log(this.palpites);
+
         for (let key of this.palpites) {
             //console.log(key, key[1].length);
             if (key[1].length < 5) {
                 key[1] += letra;
+
                 return true;
             } 
         }
         return false;
+    }
+
+    proximaPosicao(atual) {
+        console.log("atual",atual, typeof(atual));
+        
+        //se passou a ultima rodada
+        //if (atual > 65) return -1;
+
+        //ultima letra -> proxima rodada
+        //if (atual % 5 === 0) return this.posicao = atual + 6;
+
+        //mesma rodada -> proxima letra
+        return this.posicao = atual + 1;
     }
 
     //método para renderizar o board
@@ -51,7 +62,15 @@ class Wordle {
 
     //método para checar a palavra-tentativa (botao enter)
     checarTentativa(palavraTentativa) {
+        //se a letra não está inclusa -> cinza (usar includes)
 
+        //para cada letra, iterando
+        //se a letra esta inclusa na palavra E na posicao correta -> verde (ver o indexof())
+
+        //se a letra esta incluida na palavra -> amarelo
+
+
+        
     }
 
     //método para apagar letra
